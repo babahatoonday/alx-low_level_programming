d6198969a8f8d24ac5c47591af05be557177b0b2
babahatoonday <babhatoonday01@gmail.com>
2022-10-17 22:43:48 +0100

    Task 4

---
+++ 4-puts.c
@@ -0,0 +1,17 @@
+*#include <stdio.h>
+
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+    char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
+puts(Mystr);
+
+    return (0);
+}
+
