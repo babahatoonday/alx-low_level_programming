6b8067061e578447557277858899cb13e79d20b7
babahatoonday <babhatoonday01@gmail.com>
2022-10-18 01:00:58 +0100

    editted

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,16 @@
-*#include <stdio.h>
-
+#include <stdio.h>
 
 /**
  * main - Entry Point
  *
  * Return: Always 0 (success)
  */
-int main(void)
-{
-    char Mystr[] = "\"Programming is like building a multilingual puzzle";
 
-puts(Mystr);
+int main(void) 
+{
+    puts ("\"Programming is like building a multilingual puzzle");
 
     return (0);
+
 }
 
